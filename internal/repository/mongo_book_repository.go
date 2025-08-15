a28985555bbde929040e808349749184756ec344
Muciu <constantinmucioniu@gmail.com>
2025-08-15 13:21:40 +0300

    added logic for performance

--- internal/repository/mongo_book_repository.go
+++ internal/repository/mongo_book_repository.go
@@ -1,16 +1,16 @@
 package repository
 
 import (
-    "context"
-    "sync"
+	"context"
+	"sync"
 
-    "API-GO/internal/models"
-    "API-GO/internal/utils"
+	"API-GO/internal/models"
+	"API-GO/internal/utils"
 
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoBookRepository struct {
